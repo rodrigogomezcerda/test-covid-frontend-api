975d2087021426d8abea847d896ea13292b68d03
rodrigogomezcerda <rodrigo.gomez.cerda@gmail.com>
2020-08-26 16:09:17 -0400

    Updates

---
+++ precache-manifest.058314a818637fdcab75e6e694e46da1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "175789d9b892e80e72bd865d06dffcec",
+    "url": "/test-covid-frontend-api/index.html"
+  },
+  {
+    "revision": "ab6cd556fe248b647a08",
+    "url": "/test-covid-frontend-api/static/css/main.3b155eed.chunk.css"
+  },
+  {
+    "revision": "ec635b0c260afbd1234d",
+    "url": "/test-covid-frontend-api/static/js/2.3b6eb293.chunk.js"
+  },
+  {
+    "revision": "9110c923800987d12d856da8548c4502",
+    "url": "/test-covid-frontend-api/static/js/2.3b6eb293.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab6cd556fe248b647a08",
+    "url": "/test-covid-frontend-api/static/js/main.74184aed.chunk.js"
+  },
+  {
+    "revision": "b24751634fc68b44c551",
+    "url": "/test-covid-frontend-api/static/js/runtime-main.63b10350.js"
+  },
+  {
+    "revision": "567b3b9b122339706bbdfe725d8a685f",
+    "url": "/test-covid-frontend-api/static/media/social_distancing_2g0u.567b3b9b.svg"
+  }
+]);
